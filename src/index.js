168a432d8d4dff0c38eb48172b24c4afe9048580
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-04-12 22:25:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
-  // write code here
-	// dont change function name
-  
+   try {
+    const content = await fs.readFile(fileName, "utf-8");
+    return content;
+  } catch (error) {
+    console.error(`Error reading ${fileName}:`, error);
+    throw error;
+  }
 };
 
 
